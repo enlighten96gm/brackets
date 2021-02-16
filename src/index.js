2b9a39745ae02ea30f1c006072b15aa68a897d49
enlighten96gm <gm.enlightenby@gmail.com>
2021-02-16 23:48:30 +0300

    feat: test complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split('')
+    let split = arr.length
+    let a = 0
+    while (a < split) {
+        a++
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == "(" && arr[i + 1] == ")" ||
+            arr[i] == "{" && arr[i + 1] == "}" ||
+            arr[i] == "[" && arr[i + 1] == "]" ||
+            arr[i] == "|" && arr[i + 1] == "|") {
+                arr.splice(i, 2)
+            } else if (
+                arr[i] == "1" && arr[i + 1] == "2" ||
+                arr[i] == "3" && arr[i + 1] == "4" ||
+                arr[i] == "5" && arr[i + 1] == "6" ||
+                arr[i] == "7" && arr[i + 1] == "7" ||
+                arr[i] == "8" && arr[i + 1] == "8"){
+                arr.splice(i, 2)
+            }
+        }
+    }
+    console.log(arr);
+    if (arr.length === 0) {
+        return true
+    } else {
+        return false
+    }
 }
